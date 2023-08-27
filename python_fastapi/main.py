8b4d54f857903c4104c90a75e6a4f9f3949b549b
up1 <somkiat.p@gmail.com>
2023-08-28 01:11:57 +0700

    Demo with python and fast api

---
+++ python_fastapi/main.py
@@ -0,0 +1,173 @@
+import logging
+import os
+import random
+import time
+from typing import Optional
+
+import httpx
+import uvicorn
+from fastapi import FastAPI, Response
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter as OTLPSpanExporterGRPC,
+)
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+    OTLPSpanExporter as OTLPSpanExporterHTTP,
+)
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.propagate import inject
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from starlette.types import ASGIApp
+
+APP_NAME = os.environ.get("APP_NAME", "app")
+EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 8000)
+
+# jaeger-grpc, jaeger-http, otel-collector-grpc, otel-collector-http
+MODE = os.environ.get("MODE", "otel-collector-grpc")
+
+JAEGER_GRPC_ENDPOINT = os.environ.get("JAEGER_GRPC_ENDPOINT", "jaeger-collector:4317")
+JAEGER_HTTP_ENDPOINT = os.environ.get(
+    "JAEGER_HTTP_ENDPOINT", "http://jaeger-collector:4318/v1/traces"
+)
+OTEL_GRPC_ENDPOINT = os.environ.get("OTEL_GRPC_ENDPOINT", "otel-collector:4317")
+OTEL_HTTP_ENDPOINT = os.environ.get(
+    "OTEL_HTTP_ENDPOINT", "http://otel-collector:4318/v1/traces"
+)
+
+TARGET_ONE_HOST = os.environ.get("TARGET_ONE_HOST", "app-b")
+TARGET_TWO_HOST = os.environ.get("TARGET_TWO_HOST", "app-c")
+
+app = FastAPI()
+
+
+def setting_jaeger(app: ASGIApp, app_name: str, log_correlation: bool = True) -> None:
+    # Setting jaeger
+    # set the service name to show in traces
+    resource = Resource.create(attributes={"service.name": app_name})
+
+    # set the tracer provider
+    tracer = TracerProvider(resource=resource)
+    trace.set_tracer_provider(tracer)
+
+    if MODE == "jaeger-grpc":
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=JAEGER_GRPC_ENDPOINT, insecure=True)
+            )
+        )
+    elif MODE == "jaeger-http":
+        tracer.add_span_processor(
+            BatchSpanProcessor(OTLPSpanExporterHTTP(endpoint=JAEGER_HTTP_ENDPOINT))
+        )
+    elif MODE == "otel-collector-grpc":
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=OTEL_GRPC_ENDPOINT, insecure=True)
+            )
+        )
+    elif MODE == "otel-collector-http":
+        tracer.add_span_processor(
+            BatchSpanProcessor(OTLPSpanExporterHTTP(endpoint=OTEL_HTTP_ENDPOINT))
+        )
+    else:
+        # default otel-collector-grpc
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=OTEL_GRPC_ENDPOINT, insecure=True)
+            )
+        )
+
+    # override logger format which with trace id and span id
+    if log_correlation:
+        LoggingInstrumentor().instrument(set_logging_format=True)
+
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
+
+
+# Setting jaeger exporter
+setting_jaeger(app, APP_NAME)
+
+
+@app.get("/")
+async def read_root():
+    logging.error("Hello World")
+    return {"message": "Hello World from Python and FastAPI"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    logging.error("items")
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/io_task")
+async def io_task():
+    time.sleep(1)
+    logging.error("io task")
+    return "IO bound task finish!"
+
+
+@app.get("/cpu_task")
+async def cpu_task():
+    for i in range(1000):
+        _ = i * i * i
+    logging.error("cpu task")
+    return "CPU bound task finish!"
+
+
+@app.get("/random_status")
+async def random_status(response: Response):
+    response.status_code = random.choice([200, 200, 300, 400, 500])
+    logging.error("random status")
+    return {"path": "/random_status"}
+
+
+@app.get("/random_sleep")
+async def random_sleep(response: Response):
+    time.sleep(random.randint(0, 5))
+    logging.error("random sleep")
+    return {"path": "/random_sleep"}
+
+
+@app.get("/error_test")
+async def error_test(response: Response):
+    logging.error("got error!!!!")
+    raise ValueError("value error")
+
+
+@app.get("/chain")
+async def chain(response: Response):
+
+    headers = {}
+    inject(headers)  # inject trace info to header
+    logging.critical(headers)
+
+    async with httpx.AsyncClient() as client:
+        await client.get(
+            "http://localhost:8000/",
+            headers=headers,
+        )
+    async with httpx.AsyncClient() as client:
+        await client.get(
+            f"http://{TARGET_ONE_HOST}:8000/io_task",
+            headers=headers,
+        )
+    async with httpx.AsyncClient() as client:
+        await client.get(
+            f"http://{TARGET_TWO_HOST}:8000/cpu_task",
+            headers=headers,
+        )
+    logging.info("Chain Finished")
+    return {"path": "/chain"}
+
+
+if __name__ == "__main__":
+    # update uvicorn access logger format
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"][
+        "fmt"
+    ] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
+    uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
